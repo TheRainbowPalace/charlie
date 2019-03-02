6174d7ec25f37f1632231084a09f790cef4fd803
Jakob <jakobrdev@gmail.com>
2019-03-02 16:26:18 +0100

    Return bounds if sensor is outside bounds, Ignore obstacles that are outside bounds, Move Environment definition to SensorPositioning, Unify problem naming, Add deprecated SSP plotters

--- src/SensorPositioning.cs
+++ src/SensorPositioning.cs
@@ -1,288 +1,393 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Geometry;
-using ParticleSwarmOptimization;
+using Optimization;
 using Shadows;
 using Vector2 = Geometry.Vector2;
-using Environment = Shadows.Environment;
+
+
+public class Environment
+{
+  public readonly Rectangle Bounds;
+  public readonly List<Obstacle> Obstacles = new List<Obstacle>();
+
+  public Environment(Rectangle bounds)
+  {
+    Bounds = bounds;
+  }
+
+  public Environment(double x, double y, double width, double height)
+  {
+    Bounds = new Rectangle(x, y, x + width, y + height);
+  }
+
+  public double Area()
+  {
+    return Bounds.Area();
+  }
+}
+
+
+public class Obstacle
+{
+  public Vector2 Position;
+  public readonly double Size;
+
+  public Obstacle(Vector2 position, double size)
+  {
+    Position = position;
+    Size = size;
+  }
+
+  public Obstacle(double x, double y, double size)
+  {
+    Position = new Vector2(x, y);
+    Size = size;
+  }
+}
+
+
+public class Sensor : Obstacle
+{
+  public double Rotation;
+  public readonly double Range;
+  public readonly double Fov;
+
+  public Sensor(Vector2 position, double rotation, double range,
+    double fov, double size) : base(position, size)
+  {
+    Range = range;
+    Fov = fov;
+    Rotation = rotation;
+  }
+
+  public Sensor(double x, double y, double rotation, double range,
+    double fov, double size) : base(new Vector2(x, y), size)
+  {
+    Range = range;
+    Fov = fov;
+    Rotation = rotation;
+  }
+
+  /// <summary>
+  /// Get the area monitored by the sensor.
+  /// </summary>
+  /// <returns></returns>
+  public Arc AreaOfActivity()
+  {
+    return new Arc(Position, Range, Fov, Rotation - Fov / 2);
+  }
+
+  /// <summary>
+  /// Calculate the shadows for this sensor inside an environment.
+  /// </summary>
+  /// <param name="env"></param>
+  /// <returns></returns>
+  public List<Polygon> Shadows(Environment env)
+  {
+    var obstacles = new List<Circle>();
+    env.Obstacles.ForEach(o =>
+    {
+      if (o != this)
+        obstacles.Add(
+          new Circle(o.Position.X, o.Position.Y, o.Size));
+    });
+
+    return Shadows2D.Shadows(AreaOfActivity(),
+      obstacles, env.Bounds);
+  }
+
+  /// <summary>
+  /// Calculate the shadows for a list of sensors in an environment.
+  /// </summary>
+  /// <param name="sensors"></param>
+  /// <param name="env"></param>
+  /// <returns></returns>
+  public static List<Polygon> Shadows(List<Sensor> sensors,
+    Environment env)
+  {
+    var shadows = sensors.Select(sensor => sensor.Shadows(env)).ToList();
+    var result = shadows[0];
+    for (var i = 1; i < shadows.Count; i++)
+    {
+      result = Polygon.Intersection(result, shadows[i]);
+    }
+
+    return result;
+  }
+
+  /// <summary>
+  /// Calculate the shadow area for a list of sensors in an environment.
+  /// </summary>
+  /// <param name="sensors"></param>
+  /// <param name="env"></param>
+  /// <returns></returns>
+  public static double ShadowArea(List<Sensor> sensors, Environment env)
+  {
+    return Polygon.Area(Shadows(sensors, env));
+  }
+
+  /// <summary>
+  /// Calculate the shadow area for a list of shadows.
+  /// </summary>
+  /// <param name="shadows"></param>
+  /// <returns></returns>
+  public static double ShadowArea(List<Polygon> shadows)
+  {
+    return Polygon.Area(shadows);
+  }
+
+  public override string ToString()
+  {
+    return "Position: " + Position + ", Size: " + Size +
+           ", Rotation: " + Rotation + ", Range: " + Range
+           + ", Fov: " + Fov;
+  }
+}
 
 
 public class SensorPositioningProblem
 {
-    /// <summary>The problem environment.</summary>
-    public Environment Env;
-    /// <summary>A list of sensors.</summary>
-    public List<Sensor> TeamA;
-    /// <summary>A list of obstacles</summary>
-    public List<Sensor> TeamB;
+  /// <summary>The problem environment.</summary>
+  public Environment Env;
 
-    
-    public SensorPositioningProblem(
-        int sizeTeamA = 1,
-        int sizeTeamB = 1,
-        double fieldWidth = 9f,
-        double fieldHeight = 6f,
-        double playerSensorRange = 12,
-        double playerSensorFov = 56.3f,
-        double playerSize = 0.1555f
-        )
+  /// <summary>A list of sensors.</summary>
+  public List<Sensor> TeamA;
+
+  /// <summary>A list of obstacles</summary>
+  public List<Sensor> TeamB;
+
+
+  public SensorPositioningProblem(
+    int sizeTeamA = 1,
+    int sizeTeamB = 1,
+    double fieldWidth = 9f,
+    double fieldHeight = 6f,
+    double playerSensorRange = 12,
+    double playerSensorFov = 56.3f,
+    double playerSize = 0.1555f
+  )
+  {
+    var field = new Rectangle(0, 0, fieldWidth, fieldHeight);
+    Env = new Environment(field);
+
+    TeamA = new List<Sensor>();
+    TeamB = new List<Sensor>();
+
+    for (var i = 0; i < sizeTeamA; i++)
     {
-        var field = new Rectangle(0, 0, fieldWidth, fieldHeight);
-        Env = new Environment(field);
-        
-        TeamA = new List<Sensor>();
-        TeamB = new List<Sensor>();
+      var s = new Sensor(0, 0, 0, playerSensorRange,
+        playerSensorFov, playerSize);
 
-        for (var i = 0; i < sizeTeamA; i++)
-        {
-            var s = new Sensor(0, 0, 0, playerSensorRange, 
-                playerSensorFov, playerSize);
-            
-            PlaceWithoutCollision(s);
-            TeamA.Add(s);
-            Env.Obstacles.Add(s);
-        }
-        
-        for (var i = 0; i < sizeTeamB; i++)
-        {
-            var s = new Sensor(0, 0, 0, playerSensorRange, 
-                playerSensorFov, playerSize);
-
-            PlaceWithoutCollision(s);
-            TeamB.Add(s);
-            Env.Obstacles.Add(s);
-        }
+      PlaceWithoutCollision(s);
+      TeamA.Add(s);
+      Env.Obstacles.Add(s);
     }
 
-    /// <summary>
-    /// Place an object randomly without collision inside the problem
-    /// environment.
-    /// </summary>
-    /// <remarks>
-    /// Warning, this function is not deterministic and might not
-    /// terminate if Env.Field is to small.
-    /// The object can also be placed on the edges of the field.
-    /// </remarks>
-    /// <param name="o">The object to place.</param>
-    public void PlaceWithoutCollision(Obstacle o)
+    for (var i = 0; i < sizeTeamB; i++)
     {
-        var collided = true;
-        while (collided)
-        {
-            PlaceRandom(o);
-            collided = CheckCollision(o);
-        }
+      var s = new Sensor(0, 0, 0, playerSensorRange,
+        playerSensorFov, playerSize);
+
+      PlaceWithoutCollision(s);
+      TeamB.Add(s);
+      Env.Obstacles.Add(s);
+    }
+  }
+
+  /// <summary>
+  /// Place an object randomly without collision inside the problem
+  /// environment.
+  /// </summary>
+  /// <remarks>
+  /// Warning, this function is not deterministic and might not
+  /// terminate if Env.Field is to small.
+  /// The object can also be placed on the edges of the field.
+  /// </remarks>
+  /// <param name="o">The object to place.</param>
+  public void PlaceWithoutCollision(Obstacle o)
+  {
+    var collided = true;
+    while (collided)
+    {
+      PlaceRandom(o);
+      collided = CheckCollision(o);
+    }
+  }
+
+  /// <summary>
+  /// Place an object randomly inside the problem environment. This might
+  /// Cause a collision.
+  /// </summary>
+  /// <remarks>
+  /// The object can also be placed on the edges of the field.
+  /// </remarks>
+  /// <param name="o"></param>
+  public void PlaceRandom(Obstacle o)
+  {
+    var x = Pso.UniformRand(Env.Bounds.Min.X,
+      Env.Bounds.Max.X);
+    var y = Pso.UniformRand(Env.Bounds.Min.Y,
+      Env.Bounds.Max.Y);
+    o.Position = new Vector2(x, y);
+  }
+
+  /// <summary>
+  /// Check if a given object collides with any object inside the
+  /// problem environment. If the object already exists inside the environment
+  /// it won't collide with itself.
+  /// </summary>
+  /// <param name="o"></param>
+  /// <returns></returns>
+  public bool CheckCollision(Obstacle o)
+  {
+    foreach (var o2 in Env.Obstacles)
+    {
+      if (o == o2) continue;
+
+      var d = Vector2.Distance(o2.Position, o.Position);
+      if (d < o2.Size + o.Size) return true;
     }
 
-    /// <summary>
-    /// Place an object randomly inside the problem environment. This might
-    /// Cause a collision.
-    /// </summary>
-    /// <remarks>
-    /// The object can also be placed on the edges of the field.
-    /// </remarks>
-    /// <param name="o"></param>
-    public void PlaceRandom(Obstacle o)
-    {
-        var x = Pso.UniformRand(Env.Bounds.Min.X,
-            Env.Bounds.Max.X);
-        var y = Pso.UniformRand(Env.Bounds.Min.Y,
-            Env.Bounds.Max.Y);
-        o.Position = new Vector2(x, y);
-    }
-    
-    /// <summary>
-    /// Check if a given object collides with any object inside the
-    /// problem environment. If the object already exists inside the environment
-    /// it won't collide with itself.
-    /// </summary>
-    /// <param name="o"></param>
-    /// <returns></returns>
-    public bool CheckCollision(Obstacle o)
-    {
-        foreach (var o2 in Env.Obstacles)
-        {
-            if (o == o2) continue;
+    return false;
+  }
 
-            var d = Vector2.Distance(o2.Position, o.Position);
-            if (d < o2.Size + o.Size) return true;
-        }
+  /// <summary>
+  /// Place a list of sensors according 
+  /// </summary>
+  /// <remarks>
+  /// This will only change the sensors position and rotation, sensors
+  /// won't be added to the environment.
+  /// </remarks>
+  /// <param name="vector"></param>
+  /// <param name="sensors"></param>
+  public static void PlaceFromVector(double[] vector, List<Sensor> sensors)
+  {
+    for (var i = 0; i < vector.Length; i += 3)
+    {
+      sensors[i / 3].Position = new Vector2(vector[i], vector[i + 1]);
+      sensors[i / 3].Rotation = vector[i + 2];
+    }
+  }
+  
+  public static void PlaceFromVector(double[] vector, double[] lastState, 
+    List<Sensor> sensors, double moveWidth, double rotWidth)
+  {
+    for (var i = 0; i < vector.Length / 3; i += 3)
+    {
+      var v1 = new Vector2(lastState[i], lastState[i + 1]);
+      var v2 = new Vector2(vector[i], vector[i + 1]);
+      
+      Console.WriteLine("Distance: " + Vector2.Distance(v1, v2));
+      if (Vector2.Distance(v1, v2) > moveWidth)
+      {
+        var pos = v1.Move(Vector2.Gradient(v1, v2), moveWidth);
+        vector[i] = pos.X;
+        vector[i + 1] = pos.Y;
+      }
+      if (vector[i + 2] > rotWidth) vector[i + 2] = rotWidth;
+      
+      sensors[i / 3].Position = new Vector2(vector[i], vector[i + 1]);
+      sensors[i / 3].Rotation = vector[i + 2];
+    }
+  }
+ 
+  /// <summary></summary>
+  /// <param name="value"></param>
+  /// <param name="round"></param>
+  /// <returns></returns>
+  public double Normalize(double value, bool round = true)
+  {
+    if (round) return Math.Round(value / Env.Area() * 100, 2);
+    return value / Env.Area() * 100;
+  }
 
-        return false;
+  /// <summary>
+  /// Get the search space of the problem instance.
+  /// </summary>
+  /// <returns></returns>
+  public double[][] Intervals()
+  {
+    var region = Env.Bounds;
+    var intervals = new double[TeamA.Count * 3][];
+    for (var i = 0; i < intervals.Length; i += 3)
+    {
+      intervals[i] = new[] {region.Min.X, region.Max.X};
+      intervals[i + 1] = new[] {region.Min.Y, region.Max.Y};
+      intervals[i + 2] = new[] {0, 360.0};
     }
 
-    /// <summary>
-    /// Place a list of sensors according 
-    /// </summary>
-    /// <remarks>
-    /// This will only change the sensors position and rotation, sensors
-    /// won't be added to the environment.
-    /// </remarks>
-    /// <param name="vector"></param>
-    /// <param name="sensors"></param>
-    public static void PlaceFromVector(double[] vector, List<Sensor> sensors)
-    {
-        for (var i = 0; i < vector.Length; i += 3)
-        {
-            sensors[i / 3].Position = new Vector2(vector[i], vector[i + 1]);
-            sensors[i / 3].Rotation = vector[i + 2];
-        }
-    }
-    
-    /// <summary></summary>
-    /// <param name="value"></param>
-    /// <param name="round"></param>
-    /// <returns></returns>
-    public double Normalize(double value, bool round = true)
-    {
-        if (round) return Math.Round(value / Env.Area() * 100, 2);
-        return value / Env.Area() * 100;
-    }
-    
-    /// <summary>
-    /// Get the search space of the problem instance.
-    /// </summary>
-    /// <returns></returns>
-    public double[][] Intervals()
-    {
-        var region = Env.Bounds;
-        var intervals = new double[TeamA.Count * 3][];
-        for (var i = 0; i < intervals.Length; i += 3)
-        {
-            intervals[i] = new[] {region.Min.X, region.Max.X};
-            intervals[i + 1] = new[] {region.Min.Y, region.Max.Y};
-            intervals[i + 2] = new[] {0, 360.0};
-        }
-        return intervals;
-    }
+    return intervals;
+  }
+
+  public SearchSpace SearchSpace()
+  {
+    return new SearchSpace(Intervals());
+  }
 }
 
 
-/*
- Tests
- =====
- 
- ## 01
- Team 01 - 1 player
- Team 02 - 0 players 
- 
- Team 01 is updated to find the maximum area of sight.
- 
- ## 02
- Team 01 - 1 player
- Team 02 - 1 player
- 
- Team 01 is updated to find the maximum area of sight.
- Team 02 is randomly initialized on the field and does not move.
- 
- ## 03 - 08
- Team 01 - 1, 2, ..., 6 players
- Team 02 - 5 players
- 
- Team 01 is updated to find the maximum area of sight.
- Team 02 is randomly initialized on the field and does not move.
-
- */
-
-public class StaticSensorPositioning : SensorPositioningProblem
+public class SSP : SensorPositioningProblem
 {
-    public StaticSensorPositioning(
-        int sizeTeamA = 1, 
-        int sizeTeamB = 1, 
-        double fieldWidth = 9, 
-        double fieldHeight = 6, 
-        double playerSensorRange = 12, 
-        double playerSensorFov = 56.3, 
-        double playerSize = 0.1555
-    ) : base(sizeTeamA, sizeTeamB, fieldWidth, fieldHeight, 
-        playerSensorRange, playerSensorFov, playerSize) {}
+  public SSP(
+    int sizeTeamA = 1,
+    int sizeTeamB = 1,
+    double fieldWidth = 9,
+    double fieldHeight = 6,
+    double playerSensorRange = 12,
+    double playerSensorFov = 56.3,
+    double playerSize = 0.1555
+  ) : base(sizeTeamA, sizeTeamB, fieldWidth, fieldHeight,
+    playerSensorRange, playerSensorFov, playerSize)
+  {}
 
-    
-    public double FitnessFct(double[] vector)
+
+  public double FitnessFct(double[] vector)
+  {
+    PlaceFromVector(vector, TeamA);
+    foreach (var p in TeamA)
     {
-        PlaceFromVector(vector, TeamA);
-        foreach (var p in TeamA)
-        {
-            if (!CheckCollision(p)) continue;
-            Collisions++;
-            return double.PositiveInfinity;
-        }
-        return Sensor.ShadowArea(TeamA, Env);
+      if (!CheckCollision(p)) continue;
+      Collisions++;
+      return double.PositiveInfinity;
     }
+    return Sensor.ShadowArea(TeamA, Env);
+  }
 
-    public int Collisions { get; private set; }
+  public int Collisions { get; private set; }
 }
 
 
-public class DynamicSensorPositioning : SensorPositioningProblem
+public class SSPByStep : SensorPositioningProblem
 {
-    public DynamicSensorPositioning(
-        int sizeTeamA = 1, 
-        int sizeTeamB = 1, 
-        double fieldWidth = 9, 
-        double fieldHeight = 6, 
-        double playerSensorRange = 12, 
-        double playerSensorFov = 56.3, 
-        double playerSize = 0.1555
-    ) : base(sizeTeamA, sizeTeamB, fieldWidth, fieldHeight, 
-        playerSensorRange, playerSensorFov, playerSize) {}
+  private double _moveWidth;
+  private double _rotWidth;
+  
+  public SSPByStep(
+    double moveWidth,
+    int sizeTeamA = 1,
+    int sizeTeamB = 1,
+    double fieldWidth = 9,
+    double fieldHeight = 6,
+    double playerSensorRange = 12,
+    double playerSensorFov = 56.3,
+    double playerSize = 0.1555
+  ) : base(sizeTeamA, sizeTeamB, fieldWidth, fieldHeight,
+    playerSensorRange, playerSensorFov, playerSize)
+  {
+    _moveWidth = moveWidth;
+  }
+
+  public double FitnessFct(double[] vector, double[] lastState)
+  {
+    if (lastState == null) return double.PositiveInfinity;
+    
+    PlaceFromVector(vector, lastState, TeamA, _moveWidth, _rotWidth);
+    foreach (var p in TeamA)
+    {
+      if (CheckCollision(p)) return double.PositiveInfinity;
+    }
+    return Sensor.ShadowArea(TeamA, Env);
+  }
 }
-
-
-//    public static void Main()
-//    {
-//        Console.WriteLine("Test 01");
-//        Console.WriteLine("=======");
-//        Console.WriteLine();
-//
-//        var sizesTeamA = new[] {1, 2, 3, 4, 5, 6};
-//        const int testIterations = 100;
-//        const int iterations = 500;
-//        
-//        Console.WriteLine("Iterations");
-//
-//        foreach (var size in sizesTeamA)
-//        {
-//            Console.WriteLine("## " + size + "vs5");
-//            for (var j = 0; j < testIterations; j++)
-//            {
-//                var test = new Test01(size, 5);
-//                for (var i = 0; i < iterations; i++) test.Update();
-//                
-//                Console.WriteLine("i: " + (j+1) + "; " + test.Result());
-//            }
-//            Console.WriteLine();
-//        }
-
-//        Console.WriteLine("## 1vs5");
-//        var test = new Test01(sizeTeamB:5);
-//        for (var i = 0; i < iterations; i++) test.Update();
-//        Console.WriteLine();
-//        
-//        Console.WriteLine("## 2vs5");
-//        test = new Test01(sizeTeamA:2);
-//        for (var i = 0; i < iterations; i++) test.Update();
-//        Console.WriteLine();
-//        
-//        Console.WriteLine("## 3vs5");
-//        test = new Test01(sizeTeamA:3);
-//        for (var i = 0; i < iterations; i++) test.Update();
-//        Console.WriteLine();
-//        
-//        Console.WriteLine("## 4vs5");
-//        test = new Test01(sizeTeamA:4);
-//        for (var i = 0; i < iterations; i++) test.Update();
-//        Console.WriteLine();
-//        
-//        Console.WriteLine("## 5vs5");
-//        test = new Test01(sizeTeamA:5);
-//        for (var i = 0; i < iterations; i++) test.Update();
-//        Console.WriteLine();
-//        
-//        Console.WriteLine("## 5vs5");
-//        test = new Test01(sizeTeamA:6);
-//        for (var i = 0; i < iterations; i++) test.Update();
-//        Console.WriteLine();
-    
-//    }
-    
--- src/Shadows.cs
+++ src/Shadows.cs
@@ -7,146 +7,6 @@ using Vector2 = Geometry.Vector2;
 
 namespace Shadows
 {
-  public class Environment
-  {
-    public readonly Rectangle Bounds;
-    public readonly List<Obstacle> Obstacles = new List<Obstacle>();
-
-    public Environment(Rectangle bounds)
-    {
-      Bounds = bounds;
-    }
-
-    public Environment(double x, double y, double width, double height)
-    {
-      Bounds = new Rectangle(x, y, x + width, y + height);
-    }
-
-    public double Area()
-    {
-      return Bounds.Area();
-    }
-  }
-
-
-  public class Obstacle
-  {
-    public Vector2 Position;
-    public readonly double Size;
-
-    public Obstacle(Vector2 position, double size)
-    {
-      Position = position;
-      Size = size;
-    }
-
-    public Obstacle(double x, double y, double size)
-    {
-      Position = new Vector2(x, y);
-      Size = size;
-    }
-  }
-
-
-  public class Sensor : Obstacle
-  {
-    public double Rotation;
-    public readonly double Range;
-    public readonly double Fov;
-
-    public Sensor(Vector2 position, double rotation, double range,
-      double fov, double size) : base(position, size)
-    {
-      Range = range;
-      Fov = fov;
-      Rotation = rotation;
-    }
-
-    public Sensor(double x, double y, double rotation, double range,
-      double fov, double size) : base(new Vector2(x, y), size)
-    {
-      Range = range;
-      Fov = fov;
-      Rotation = rotation;
-    }
-
-    /// <summary>
-    /// Get the area monitored by the sensor.
-    /// </summary>
-    /// <returns></returns>
-    public Arc AreaOfActivity()
-    {
-      return new Arc(Position, Range, Fov, Rotation - Fov / 2);
-    }
-
-    /// <summary>
-    /// Calculate the shadows for this sensor inside an environment.
-    /// </summary>
-    /// <param name="env"></param>
-    /// <returns></returns>
-    public List<Polygon> Shadows(Environment env)
-    {
-      var obstacles = new List<Circle>();
-      env.Obstacles.ForEach(o =>
-      {
-        if (o != this)
-          obstacles.Add(
-            new Circle(o.Position.X, o.Position.Y, o.Size));
-      });
-
-      return Shadows2D.Shadows(AreaOfActivity(), 
-        obstacles, env.Bounds);
-    }
-
-    /// <summary>
-    /// Calculate the shadows for a list of sensors in an environment.
-    /// </summary>
-    /// <param name="sensors"></param>
-    /// <param name="env"></param>
-    /// <returns></returns>
-    public static List<Polygon> Shadows(List<Sensor> sensors,
-      Environment env)
-    {
-      var shadows = sensors.Select(sensor => sensor.Shadows(env)).ToList();
-      var result = shadows[0];
-      for (var i = 1; i < shadows.Count; i++)
-      {
-        result = Polygon.Intersection(result, shadows[i]);
-      }
-
-      return result;
-    }
-
-    /// <summary>
-    /// Calculate the shadow area for a list of sensors in an environment.
-    /// </summary>
-    /// <param name="sensors"></param>
-    /// <param name="env"></param>
-    /// <returns></returns>
-    public static double ShadowArea(List<Sensor> sensors, Environment env)
-    {
-      return Polygon.Area(Shadows(sensors, env));
-    }
-
-    /// <summary>
-    /// Calculate the shadow area for a list of shadows.
-    /// </summary>
-    /// <param name="shadows"></param>
-    /// <returns></returns>
-    public static double ShadowArea(List<Polygon> shadows)
-    {
-      return Polygon.Area(shadows);
-    }
-
-    public override string ToString()
-    {
-      return "Position: " + Position + ", Size: " + Size +
-             ", Rotation: " + Rotation + ", Range: " + Range
-             + ", Fov: " + Fov;
-    }
-  }
-
-
   public static class Shadows2D
   {
     public static List<Polygon> UnseenArea(Arc sensor, Rectangle bounds)
@@ -210,13 +70,12 @@ namespace Shadows
       Arc sensor, List<Circle> obstacles, Rectangle bounds)
     {
       if (!bounds.Contains(sensor.Position))
-        throw new ArgumentException("Sensor is outside bounds.");
+        return new List<Polygon> {bounds.ToPolygon()};
 
       var polygons = new List<Polygon>();
       foreach (var obstacle in obstacles)
       {
-        if (!bounds.Contains(obstacle.Position))
-          throw new ArgumentException("Obstacle is outside bounds.");
+        if (!bounds.Contains(obstacle.Position)) continue;
 
         polygons.Add(HiddenArea(sensor.Position, obstacle, bounds));
       }
