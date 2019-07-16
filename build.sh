55469007dc0d7731f9bb253ccfdf2face672f04e
Jakob <jakobrdev@gmail.com>
2019-07-16 14:09:38 +0200

    Add development releases

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ OUTPUTDIR="bin/Debug/net471"
 
 msbuild charlie.csproj -nologo -verbosity:quiet
 
+# Update resources
 rm -fr $OUTPUTDIR/resources
 cp -r resources $OUTPUTDIR
+
+# Update recent build
+zip releases/Charlie-dev.zip -r -q bin/Debug/net471/
--- pack_release.sh
+++ pack_release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ./build_release.sh
+./build_mac_os.sh
 VERSIONLINE=$(grep -E 'Version = "(\d+\.?){3}"' src/Charlie.cs)
 VERSION=$(echo $VERSIONLINE |grep -Eo "(\d+\.?){3}")
 zip "releases/charlie-v$VERSION.zip" -r -q bin/Charlie bin/Charlie.app
