811eaea6879a67dffde79e21a06a587874e15d3a
Jakob <jakobrdev@gmail.com>
2020-11-05 01:16:33 +0100

    Fix pack_release script is using old release folder name

--- pack_release.sh
+++ pack_release.sh
@@ -3,5 +3,5 @@
 ./build_mac_os.sh
 VERSIONLINE=$(grep -E 'Version = "(\d+\.?){3}"' src/Charlie.cs)
 VERSION=$(echo $VERSIONLINE |grep -Eo "(\d+\.?){3}")
-zip "releases/charlie-v$VERSION.zip" -r -q bin/Charlie bin/Charlie.app
-echo "Created releases/charlie-v$VERSION.zip"
+zip "release/charlie-v$VERSION.zip" -r -q bin/Charlie bin/Charlie.app
+echo "Created release/charlie-v$VERSION.zip"
